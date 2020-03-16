21bdffc0c9a2cc3da445d92d02e19c37a57fdbcf
pvial <pvial@thin>
2020-03-16 08:17:06 -0700

    hx

---
+++ README.md
@@ -0,0 +1,5 @@
+# hx
+
+***  Warning this hash function has not been fully vetted yet and is still in development
+
+hx is a 256 bit hash function.  It is designed to be as fast or faster than the MD5 algorithm.  It's purpose is to provide an alternative to MD5 in applications where pre-image resistance and collision resistance do not play a role.  It can be used to easily and uniquely fingerprint files.
---
+++ src/hx_sum.c
@@ -0,0 +1,31 @@
+#include "hx.c"
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    unsigned char *password;
+    unsigned char *salt = "DarkCastleSui";
+    unsigned char D[32] = {0};
+    unsigned char *buf;
+    char digest[32*2+1];
+    FILE *infile;
+    if (argc != 2) {
+        printf("hxsum <input file>\n");
+        exit(1);
+    }
+    if (access(argv[1], F_OK) == -1 ) {
+        printf("%s not found\n", argv[1]);
+        exit(1);
+    }
+    infile = fopen(argv[1], "rb");
+    fseek(infile, 0, SEEK_END);
+    long long fsize = ftell(infile);
+    fseek(infile, 0, SEEK_SET);
+    buf = (unsigned char *)malloc(fsize);
+    fread(buf, 1, fsize, infile);
+    fclose(infile);
+    ganja_digest(buf, fsize, D, salt, 12);
+    for (int x = 0; x < 32; x++) {
+        sprintf(&digest[x*2], "%02x", D[x]);
+    }
+    printf("%s\n", digest);
+}
